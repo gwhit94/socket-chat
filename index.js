d45206acba3cfa35d86780de7c83a760224eb99b
Graham W <greysnaa@gmail.com>
2018-07-24 00:06:03 -0500

    adding a change from the feature branch

--- index.js
+++ index.js
@@ -3,24 +3,48 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var chatHistory = [];
-
-var clients = [];
+var onlineUsers = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// Broadcast a message to connected users when someone connects or disconnects
+// - Add support for nicknames
+// - Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.
+// - Add “{user} is typing” functionality
+// - Show who’s online
 io.on('connection', function(socket){
-  console.log('a user connected');
-  io.emit('user connected', 'a new user connected!');
+  // Alerts console that an unnamed user has connected and displays their ID
+  console.log(`An unnamed User (ID ${socket.id}) has connected.`);
+  // When the User creates a user name it is passed here with their unique ID
+  socket.on('new user', function(newUser){
+    var onlineUser = {
+      username: newUser,
+      id: socket.id,
+    };
+    onlineUsers.push(onlineUser);
+    // Alerts the chat and the server console of the Username chosen by the user
+    console.log(newUser + " joined the chat!");
+    io.emit('user connected', `${newUser} joined the chat!`);
+  });
+
   socket.broadcast.emit('hi');
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+  // Receives then Sends chat messages sent from one client back to client side
+  socket.on('chat message', function(received){
+    var msg = JSON.parse(received);
+    var time = new Date(msg.date)
+    var serverTime = time.toLocaleTimeString();
+    msg.date = serverTime;
+    var text = msg.text;
+    console.log( serverTime + ': message: ' + text);
+    msg = JSON.stringify(msg);
     io.emit('chat message', msg);
   });
+  // Alerts console and client chat that a user has disconnected
   socket.on('disconnect', function(){
     console.log('user disconnected');
-    io.emit('user disconnected', 'a user disconnected!');
+    io.emit('user disconnected', `A User (ID ${socket.id}) has left the chat!`);
   });
   io.emit('some event', { for: 'everyone' });
 
